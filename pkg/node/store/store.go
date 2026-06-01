2fbc72e695d38856209937f7399a83dd7a86006f
Paul Chaignon <paul.chaignon@gmail.com>
2026-06-01 13:04:35 +0200

    kvstore: Don't set fake pod CIDRs for remote nodes
    This commit is the kvstore mode counterpart of 15685580c9a4 ("ipam: Fix
    invalid PodCIDR in CiliumNode in ENI/Azure/MultiPool mode") which fixed
    this bug in the CRD mode case.
    
    In kvstore mode, we are currently announcing the internal,
    auto-generated pod CIDRs of the local node to all remote nodes via the
    kvstore. In IPAM modes without a pod CIDR (ex., ENI, Azure,
    AlibabaCloud), this results in a fake pod CIDR (10.x.0.0/16) being
    announced to other nodes.
    
    Since 731e182e166d ("node/manager: Store node allocation CIDRs
    into ipcache"), this pod CIDR is written in the ipcache, as a fallback
    entry. That fallback ipcache entry can cause issues when the fake CIDR
    clashes with existing entities outside the cluster.
    
    For example, it will cause packet drops and prevent connectivity to such
    entities if IPsec or WireGuard is enabled. In those cases, the fallback
    ipcache entry carries the encryption key reference. Outgoing packets
    will thus be marked for encryption but the corresponding IPsec or
    WireGuard configuration will be missing, leading to packet drops.
    
    Fixes: 731e182e166d ("node/manager: Store node allocation CIDRs into ipcache")
    Fixes: 15685580c9a4 ("ipam: Fix invalid PodCIDR in CiliumNode in ENI/Azure/MultiPool mode")
    Reported-by: Odin Ugedal <odin@ugedal.com>
    Suggested-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- pkg/node/store/store.go
+++ pkg/node/store/store.go
@@ -10,9 +10,11 @@ import (
 	"path"
 
 	"github.com/cilium/cilium/pkg/defaults"
+	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
 	"github.com/cilium/cilium/pkg/time"
 )
@@ -167,7 +169,22 @@ func (nr *NodeRegistrar) RegisterNode(ctx context.Context, logger *slog.Logger, 
 		return err
 	}
 
-	err = nodeStore.UpdateLocalKeySync(ctx, n.DeepCopy())
+	node := n.DeepCopy()
+	switch option.Config.IPAM {
+	case ipamOption.IPAMKubernetes, ipamOption.IPAMClusterPool, ipamOption.IPAMMultiPool:
+		// Keep the PodCIDRs as they are valid.
+	default:
+		// Strip the PodCIDRs on non-podCIDR based IPAM modes (e.g. ENI, Azure, AlibabaCloud). In
+		// those cases, the IPv4/IPv6AllocRange is auto-generated and otherwise unused, so it does
+		// not make sense to copy it to the kvstore.
+		// See NodeDiscovery.mutateNodeResource() for the equivalent CRD mode logic.
+		node.IPv4AllocCIDR = nil
+		node.IPv4SecondaryAllocCIDRs = nil
+		node.IPv6AllocCIDR = nil
+		node.IPv6SecondaryAllocCIDRs = nil
+	}
+
+	err = nodeStore.UpdateLocalKeySync(ctx, node)
 	if err != nil {
 		nodeStore.Release()
 		return err
